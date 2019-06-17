0381dc4877f52feb6da5259032b7026ce2d2dde4
Ifeanyi Osinakayah Joseph <ifeanyi.Osinakayah@cousant.com>
2019-06-17 12:09:05 +0100

    Completed models

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,9 @@
 class Airport < ApplicationRecord
+  before_save { email.downcase! }
+
+  belongs_to :booking
+  has_many :flights, through: :bookings
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
