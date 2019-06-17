fbb32de246f578347931771f9c1c8755aeecac4d
Ifeanyi Osinakayah Joseph <ifeanyi.Osinakayah@cousant.com>
2019-06-17 12:34:47 +0100

    Implemented views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root'flights#index', as: 'flights'
+  get 'flights/index'
+  resources :bookings, only: [:new, :create, :index, :show]
 end
